542cbdb9438a2b82cfea47f8e40f2c5d1e0e757d
bRENDON-lEE10 <144931412+bRENDON-lEE10@users.noreply.github.com>
2023-10-30 08:58:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,30 +5,17 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   // Inputs
-  let r = document.getElementById("r-in").Value;
-  let h = document.getElementById("h-in").value;
+  let r = +document.getElementById("r-in").value;
+  let h = +document.getElementById("h-in").value;
 
   // Process
-  let total = Math.PI * r ** 2 * h;
+  let volume = Math.PI * r ** 2 * h;
   console.log(Math.PI * r ** 2 * h);
 
+  let Surface = 2 * Math.PI * r * h + 2 * Math.PI * r * r;
+  console.log(2 * Math.PI * r * h + 2 * Math.PI * r * r);
+
   // Output
-  document.getElementById("output").innerHTML = total;
+  document.getElementById("output1").innerHTML = volume;
+  document.getElementById("output2").innerHTML = Surface;
 }
-
-class Cylinder {
-  constructor(radius, height) {
-    this.radius = radius;
-    this.height = height;
-  }
-
-  getVolume() {
-    const radius = this.radius;
-    const height = this.height;
-    const volume = Math.PI * radius * radius * height;
-    return volume.toFixed(4);
-  }
-}
-
-const cylinder = new cylinder(5, 10);
-console.log(cylinder.getVolume());
